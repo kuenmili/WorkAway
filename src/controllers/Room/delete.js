9a92f792640c0eac70436757570cd547605e3ba9
Lucam13 <lucc_88@hotmail.com>
2023-06-17 14:49:36 -0300

    fixes first step

--- src/controllers/Room/delete.js
+++
@@ -1,8 +0,0 @@
-const { Room } = require('../../models/Room');
-
-const deleteRoom = async (id) => {
-    const deletedRoom = await Room.findByIdAndDelete(id, { new: true });
-    return deletedRoom;
-};
-
-module.exports = deleteRoom;
