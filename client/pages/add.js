0bff95379d53f811b2e84ca889fbba17cf26a3e6
camm2210 <camilodiaza37@gmail.com>
2023-06-21 20:31:05 -0500

    first steps

---
+++ client/pages/add.js
@@ -0,0 +1,26 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Container from "../components/container";
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      <Container className="flex flex-wrap ">
+        <div className="max-w-2xl mx-auto mt-8 p-6 bg-white rounded-lg shadow-2xl dark:bg-gray-200">
+          <h2 className="text-4xl font-bold mb-4 text-center dark:text-black">
+            añadir un coworking
+          </h2>
+          <form>
+            <label></label>
+          </form>
+          <div className="absolute inset-x-0 bottom-0 ">
+            <Footer />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default page;
