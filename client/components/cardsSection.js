e31d3eba89b0770ba3472dbd4ce2c14a23118c54
Lucam13 <lucc_88@hotmail.com>
2023-06-23 16:37:57 -0300

    Back- front connection. CoworkSpaces ok

--- client/components/cardsSection.js
+++ client/components/cardsSection.js
@@ -22,7 +22,7 @@ export default function cardsSection() {
 
         <div className="container py-12 flex ">
           <div className="grid lg:grid-cols-3 gap-12 ">
-            {coworkSpaces.length && coworkSpaces?.map((card) => (
+            {coworkSpaces?.map((card) => (
                 <div className ="shadow-lg rounded-lg hover:scale-110 transition duration-300" key={card._id}>
                   <Link href={`/detail/${card.id}`}>
                   
@@ -44,9 +44,10 @@ export default function cardsSection() {
                     </Link>
                    <div className='p-5'>
                     <h3 className='text-3xl font-bold text-slate-700 mb-3 dark:text-white'>{card.name}</h3>
-                    <p className='text-lg font-normal text-gray-600 dark:text-white'>{card.about}</p>
+                    <p className='text-lg font-bold text-gray-700 dark:text-white mb-1'>{card.location}</p>
+                    <p className='text-lg font-normal text-gray-600 dark:text-white line-clamp-5'>{card.about} </p><Link href={`/detail/${card.id}`}>Ver mas</Link>
                     <Rating stars={Array(5).fill(card.rating)} className=""/>
-                    <p className='text-2xl font-bold text-gray-800 text-center dark:text-white'>{`${card.price}usd`}/dia</p>
+                    <p className='text-2xl font-bold text-gray-800 text-center dark:text-white mt-2'>{`${card.price}usd`}/dia</p>
                   </div>
                 </div>
             ))}
@@ -65,7 +66,7 @@ const Rating = ({ stars }) => {
         <svg 
           key = {idx}
           aria-hidden = "true"
-          className = {`w-5 h-5 ${star <= idx ?  'text-gray-300' : 'text-yellow-400' } `}
+          className = {`w-5 h-5 ${star <= idx ?  'text-gray-300' : 'text-yellow-400' } mr-2 mb-2 mt-3 `}
           fill = "currentColor"
           viewBox='0 0 20 20'
           xlmns = "http://www.w3.org/2000/svg">
--- client/components/filters.js
+++ client/components/filters.js
@@ -46,7 +46,12 @@ export default function Filters() {
                     </label>
                     <select onChange={handleOnChange} value={filters.location} className="w-24 px-2 border-2 cursor-pointer rounded-full shadow-lg duration-300 active:outline-none focus:outline-none focus:ring dark:text-white">
                     <option value></option>
-                    <option value={1}>1</option>
+                    <option value={1}>Buenos Aires</option>
+                    <option value={1}>Cordoba</option>
+                    <option value={1}>Rosario</option>
+                    <option value={1}>Salta</option>
+                    <option value={1}>Mendoza</option>
+                    <option value={1}>Entre Rios</option>
                     </select>
             </div>
             <div className="flex items-baseline ">
@@ -55,7 +60,7 @@ export default function Filters() {
                 </label>
                 <select onChange={handleOnChange} value={filters.capacity} className="w-24 px-2 border-2 cursor-pointer rounded-full shadow-lg duration-300 active:outline-none focus:outline-none focus:ring dark:text-white">
                     <option value></option>
-                    <option value>0 a 5 personas</option>
+                    <option value>1 a 5 personas</option>
                     <option value>10 a 15 personas</option>
                     <option value>15 a 20 personas</option>
                 </select>
@@ -67,8 +72,12 @@ export default function Filters() {
                 <select onChange={handleOnChange} value={filters.services} className="w-24 px-2 border-2 cursor-pointer rounded-full shadow-lg duration-300 active:outline-none focus:outline-none focus:ring dark:text-white">
                     <option value></option>
                     <option value>Cafeteria</option>
-                    <option value>Cafeteria</option>
-                    <option value>Cafeteria</option>
+                    <option value>Pet Friendly</option>
+                    <option value>Estacionamiento</option>
+                    <option value>Bike Parking</option>
+                    <option value>Buffet</option>
+                    <option value>Natural Light</option>
+                    <option value>Quiet Music</option>
                 </select>
             </div>
             <div className="flex items-baseline ">
