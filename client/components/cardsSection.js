70f35e8367a174e75cd88f1139c9853cac57baa7
Lucam13 <lucc_88@hotmail.com>
2023-06-25 17:50:29 -0300

    Bugs Clear2

--- client/components/cardsSection.js
+++ client/components/cardsSection.js
@@ -47,7 +47,7 @@ export default function cardsSection() {
                     <h3 className='text-3xl font-bold text-slate-700 mb-3 dark:text-white'>{card.name}</h3>
                     <p className='text-lg font-bold text-gray-700 dark:text-white mb-1'>{card.location}</p>
                     <p className='text-lg font-normal text-gray-600 dark:text-white line-clamp-5'>{card.about} </p><Link href={`/detail/${card._id}`}>Ver mas</Link>
-                    <p className='text-lg font-normal text-gray-600 dark:text-white line-clamp-5'>{card.services} </p>
+                     
                     <Rating stars={Array(5).fill(card.score)} className=""/>
                     <p className='text-2xl font-bold text-gray-800 text-center dark:text-white mt-2'>{`${card.price}usd`}/dia</p>
                   </div>
