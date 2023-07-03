7826c24764064b48d7890041311ce82ed347e98c
DNereaNicolau <85116192+Dalmanicolau@users.noreply.github.com>
2023-07-03 18:54:32 -0300

    auth update

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useRouter } from 'next/router';
-import axios from "axios";
 import { FaGoogle, FaFacebook, FaApple } from 'react-icons/fa';
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
