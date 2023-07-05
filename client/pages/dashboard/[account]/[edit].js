46f14911edb8be1c6a2363d0322b241dd2d8bc8b
camm2210 <camilodiaza37@gmail.com>
2023-07-05 07:44:04 -0500

    Profile Updates

--- client/pages/dashboard/[account]/[edit].js
+++ client/pages/dashboard/[account]/[edit].js
@@ -14,36 +14,36 @@ import React from "react";
 import Layout from "../../../components/layout";
 import { BsFillPencilFill } from "react-icons/bs";
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const Profile = ({
-  profile_image,
-  name,
-  cuit,
-  email,
-  address,
-  cellphone_number,
-  id,
-}) => {
+const Profile = ({ profile_image }) => {
+  const bussinesProfile = useSelector((state) => state.auth?.user);
+
+  const dispatch = useDispatch();
+
   const [inputValue, setInputValue] = useState({
-    name: "Palermo CoWorks",
-    email: "palermo_cowork@gmail.com",
-    password: "1234567",
+    name: "",
+    email: "",
+    password: "",
     profile_image: "",
-    address: "Av. del Libertador 1745, Palermo, Buenos Aires",
-    cuit: "2334782302",
-    cellphone_number: "3845988263",
+    address: "",
+    cuit: "",
+    cellphone_number: "",
   });
 
   const [isEditing, setIsEditing] = useState(false);
 
   const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+    setInputValue({
+      ...inputValue,
+      [event.target.name]: event.target.value,
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setIsEditing(false);
-    // Lógica para enviar los datos del formulario
+    dispatch(updateBusiness(inputValue));
   };
 
   const handleEditClick = () => {
@@ -55,23 +55,6 @@ const Profile = ({
       <Layout>
         <div className="flex justify-evenly -mt-12">
           <div className="bg-white rounded-lg  w-full md:w-2/3 lg:w-1/2 xl:w-1/3 p-8 dark:bg-[#26272c] shadow-2xl shadow-[rgba(0, 0, 0, 0.16)]">
-            <div className="relative flex justify-center">
-              <div className="rounded-full overflow-hidden w-32 h-32 md:w-48 md:h-48 xl:w-56 xl:h-56">
-                <img
-                  className="w-full h-full object-cover"
-                  src={profile_image}
-                  alt=""
-                  loading="eager"
-                />
-                <button
-                  type="button"
-                  onClick=""
-                  className="absolute bottom-0 right-0 w-8 h-8 rounded-full bg-indigo-700 text-white flex items-center justify-center mb-1 mr-24"
-                >
-                  <FontAwesomeIcon icon={faPencilAlt} />
-                </button>
-              </div>
-            </div>
             <div className="mt-4 text-center mt-5">
               <h1>Nombre</h1>
               <div className="flex items-center">
@@ -81,7 +64,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.name}
+                  value={bussinesProfile?.name}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -101,7 +84,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.email}
+                  value={bussinesProfile?.email}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -121,7 +104,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.address}
+                  value={bussinesProfile?.address}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -141,7 +124,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.password}
+                  value={bussinesProfile?.password}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -161,7 +144,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.cuit}
+                  value={bussinesProfile?.cuit}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -181,7 +164,7 @@ const Profile = ({
                 />
                 <input
                   type="text"
-                  value={inputValue.cellphone_number}
+                  value={bussinesProfile?.cellphone_number}
                   onChange={handleInputChange}
                   className="block w-full dark:bg-transparent dark:text-white bg-white border border-gray-300 rounded-md py-2 px-4 mt-1 ml-4 mr-8 focus:outline-none focus:border-indigo-600  hover:border-indigo-600 "
                   readOnly={isEditing ? false : true}
@@ -194,7 +177,8 @@ const Profile = ({
             </div>
             <div className="mt-4 flex justify-center pt-3">
               <Link
-                href={`/users/${id}/edit`}
+                onClick={handleSubmit}
+                href=""
                 className="px-6 py-2 text-white bg-indigo-700 border border-transparent rounded-md hover:bg-blue-700"
               >
                 Guardar
