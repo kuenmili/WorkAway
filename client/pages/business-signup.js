ea8cb9a351497f62d1658512e98d50d368a8cf08
kuenmili <122223943+kuenmili@users.noreply.github.com>
2023-07-06 12:41:42 -0300

    business sign up

--- client/pages/business-signup.js
+++ client/pages/business-signup.js
@@ -1,62 +1,81 @@
-import { useDispatch } from 'react-redux';
-import React, { useState } from 'react';
-import { useRouter} from 'next/router';
-import { createBusiness } from '../redux/actions/business';
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { createBusiness } from "../redux/actions/business";
 import Navbar from "../components/navBusiness";
 import Footer from "../components/footer";
 import { createBusinessValidation } from "../components/validations";
 
 const signupBusiness = () => {
-  const dispatch = useDispatch()
-  const history = useRouter()
-  
+  const dispatch = useDispatch();
+  const history = useRouter();
 
   const [input, setInput] = useState({
-    name:'',
-    cuit:'',
-    email:'',
-    password:'',
-    cellphone_number:'',
-    address:''
+    name: "",
+    cuit: "",
+    email: "",
+    password: "",
+    cellphone_number: "",
+    address: "",
   });
 
   const [errors, setError] = useState({
-    name:'',
-    cuit:'',
-    email:'',
-    password:'',
-    cellphone_number:'',
-    address:''
+    name: "",
+    cuit: "",
+    email: "",
+    password: "",
+    cellphone_number: "",
+    address: "",
   });
 
   const handlerChange = (event) => {
     setInput({
-        ...input,
-        [event.target.name] : event.target.value
+      ...input,
+      [event.target.name]: event.target.value,
     });
 
-    setError(createBusinessValidation({ ...input, [event.target.name]: event.target.value}));
-  }
+    setError(
+      createBusinessValidation({
+        ...input,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
 
+  const handlerSubmit = (event) => {
+    event.preventDefault();
 
-const handlerSubmit = (event) => {
-  event.preventDefault();
+    // ----------------------------------------------
+    // Verificar campos requeridos
+    if (Object.values(input).some((value) => value === "")) {
+      // Al menos un campo requerido está vacío, mostrar mensaje de error
+      alert("Por favor, completa todos los campos requeridos.");
+      setError({
+        name: "Campo requerido",
+        cuit: "Campo requerido",
+        email: "Campo requerido",
+        password: "Campo requerido",
+        cellphone_number: "Campo requerido",
+        address: "Campo requerido",
+      });
+      return;
+    }
 
-  dispatch(createBusiness(input));
+    dispatch(createBusiness(input));
 
-  alert("Se creó tu perfil con exito!");
+    alert("Se creó tu perfil con exito!");
 
-  setInput({
-    name:'',
-    cuit:0,
-    email:'',
-    password:'',
-    cellphone_number:0,
-    address:''
-  });
+    setInput({
+      name: "",
+      cuit: 0,
+      email: "",
+      password: "",
+      cellphone_number: 0,
+      address: "",
+    });
 
-  history.push('/dashboard');
-}
+    history.push("/dashboard");
+  };
 
   return (
     <div>
@@ -75,27 +94,35 @@ const handlerSubmit = (event) => {
             {/*Name input */}
             <div className="flex flex-col">
               <input
-                value= {input.name}
+                value={input.name}
                 name="name"
                 onChange={handlerChange}
                 type="text"
                 placeholder="Nombre"
                 className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
               />
-              {errors.name ? <p>{errors.name}</p>:''}
+              {errors.name ? (
+                <p className="text-red-500 ml-20">{errors.name}</p>
+              ) : (
+                ""
+              )}
             </div>
             {/*CUIT input */}
             <div className="mt-6">
               <div className="flex flex-col">
                 <input
                   type="number"
-                  value= {input.cuit}
+                  value={input.cuit}
                   name="cuit"
                   onChange={handlerChange}
                   placeholder="CUIT"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors.cuit ? <p>{errors.cuit}</p>:''}
+                {errors.cuit ? (
+                  <p className="text-red-500 ml-20">{errors.cuit}</p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
 
@@ -104,13 +131,17 @@ const handlerSubmit = (event) => {
               <div className="flex flex-col">
                 <input
                   type="email"
-                  value= {input.email}
+                  value={input.email}
                   name="email"
                   onChange={handlerChange}
                   placeholder="nombre@direccion.com"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors.email ? <p>{errors.email}</p>:''}
+                {errors.email ? (
+                  <p className="text-red-500 ml-20">{errors.email}</p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
 
@@ -119,13 +150,17 @@ const handlerSubmit = (event) => {
               <div className="flex flex-col">
                 <input
                   type="password"
-                  value= {input.password}
+                  value={input.password}
                   name="password"
                   onChange={handlerChange}
                   placeholder="Contraseña"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors.password ? <p>{errors.password}</p>:''}
+                {errors.password ? (
+                  <p className="text-red-500 ml-20">{errors.password}</p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
 
@@ -134,13 +169,19 @@ const handlerSubmit = (event) => {
               <div className="flex flex-col">
                 <input
                   type="tel"
-                  value= {input.cellphone_number}
+                  value={input.cellphone_number}
                   name="cellphone_number"
                   onChange={handlerChange}
                   placeholder="Numero de teléfono"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors.cellphone_number ? <p>{errors.cellphone_number}</p>:''}
+                {errors.cellphone_number ? (
+                  <p className="text-red-500 ml-20">
+                    {errors.cellphone_number}
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
 
@@ -149,13 +190,17 @@ const handlerSubmit = (event) => {
               <div className="flex flex-col">
                 <input
                   type="text"
-                  value= {input.address}
+                  value={input.address}
                   name="address"
                   onChange={handlerChange}
                   placeholder="Dirección"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors.address ? <p>{errors.address}</p>:''}
+                {errors.address ? (
+                  <p className="text-red-500 ml-20">{errors.address}</p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
 
@@ -207,4 +252,3 @@ const handlerSubmit = (event) => {
 };
 
 export default signupBusiness;
-
