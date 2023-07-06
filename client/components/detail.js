832389d483345eeb222e93cf2711b0e7dd9dc7a7
Camilo Diaz <camilodiaza37@gmail.com>
2023-07-06 10:02:03 -0500

    Update detail (block)

--- client/components/detail.js
+++ client/components/detail.js
@@ -38,7 +38,16 @@ const Detail = ({
     }
     return stars;
   };
-  console.log(reviews);
+
+
+ const handleReserveClick = () => {
+    if (user) {
+      router.push("https://workaway.vercel.app/booking");
+    } else {
+      router.push("https://workaway.vercel.app/business-login");
+    }
+  };
+
   return (
     <div className="w-screen bg-gray-100 dark:bg-gray-900">
       <Container className="mx-auto py-10 px-2 max-w-full">
@@ -160,12 +169,12 @@ const Detail = ({
                 </div>
 
                 <div className="p-4 flex justify-start items-center space-x-4 mt-8 mr-12">
-                  <Link
-                    href="/booking"
+                  <button
                     className="px-6 py-2 text-white bg-indigo-800 rounded-md"
+                    onClick={handleReserveClick}
                   >
-                    Reservar
-                  </Link>
+                    reservar
+                  </button>
                   <Link
                     href="/home"
                     className="px-6 py-2 text-white bg-indigo-800 rounded-md"
