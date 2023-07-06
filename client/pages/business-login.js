2e341c3e007974858a30f19bc48cafe6602e978a
programador5781 <programador5781@gmail.com>
2023-07-05 21:42:28 -0500

    update business-login

--- client/pages/business-login.js
+++ client/pages/business-login.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import Navbar from "../components/navBusiness";
 import Footer from "../components/footer";
 import { businessValidations } from "../components/validations";
-import { FaGoogle, FaFacebook, FaApple } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 import { login } from "../redux/actions/auth";
 
 const LoginBusiness = () => {
@@ -44,6 +44,8 @@ const LoginBusiness = () => {
     );
   };
 
+  
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -52,6 +54,14 @@ const LoginBusiness = () => {
       password: "",
       error: "",
     });
+    if (!businessData.email || !businessData.password) {
+      setErrors({
+        email: businessData.email ? '' : 'Este campo es requerido',
+        password: businessData.password ? '' : 'Este campo es requerido',
+        error: '',
+      });
+      return;
+    }
 
     dispatch(login(businessData));
   };
@@ -98,7 +108,7 @@ const LoginBusiness = () => {
                 placeholder="nombre@direccion.com"
                 className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
               />
-              {errors?.email ? <p>{errors.email}</p> : ""}
+              {errors?.email ? <p className="text-red-500 mt-1 ml-20">{errors.email}</p> : ""}
             </div>
 
             {/*Password input */}
@@ -113,7 +123,7 @@ const LoginBusiness = () => {
                   placeholder="ingresa tu contraseña"
                   className="w-3/4 mx-auto bg-white border border-indigo-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-600 dark:text-black"
                 />
-                {errors?.password ? <p>{errors.password}</p> : ""}
+                {errors?.password ? <p className="text-red-500 mt-1 ml-20">{errors.password}</p> : ""}
               </div>
             </div>
 
@@ -123,22 +133,23 @@ const LoginBusiness = () => {
                 // disabled= {errors?.email || errors?.password}
                 type="submit"
                 onClick={handleSubmit}
-                className="w-40 mt-6 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-2 rounded-md focus:outline-none"
+                className="w-3/4 mt-6 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-2 rounded-md focus:outline-none"
               >
                 Ingresa
               </button>
             </div>
             <div>
-              <div>
+              <div className="flex justify-center">
                 <button
-                  className="ml-72 mt-5"
+                  className="mt-4 w-3/4 bg-white border border-indigo-600 hover:bg-gray-300 dark:hover:bg-gray-300 text-indigo-600 font-bold py-2 px-4 rounded-md flex items-center justify-center focus:outline-none "
                   onClick={(e) => {
                     e.preventDefault();
                     window.location.href = "http://localhost:3001/auth/google";
                   }}
                 >
                   {" "}
-                  {<FaGoogle size={24} color="#DB4437" />}{" "}
+                  {<FcGoogle size={24} color="#DB4437" className="mr-4"/>}{" "}
+                  Conéctate con Google
                 </button>
               </div>
             </div>
