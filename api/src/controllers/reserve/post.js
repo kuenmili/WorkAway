b6b332e79c9c9809aa4142dc670045c9b3b4988d
kuenmili <122223943+kuenmili@users.noreply.github.com>
2023-07-06 11:13:50 -0300

    mail

--- api/src/controllers/reserve/post.js
+++ api/src/controllers/reserve/post.js
@@ -51,6 +51,7 @@ const mailOptions = {
           <p className="text-base mb-4">¡Gracias por unirte a nosotros! Te damos la bienvenida a nuestra plataforma de reservas de espacios remotos.</p>
           <p className="text-base mb-4">Esta es la informacion de su reserva de espacio en WorkAway</p>
           <p className="text-base mb-4">Espacio: ${cowork.name}</p>
+          <p className="text-base mb-4">Ubicacion: ${cowork.location} </p>
           <p className="text-base mb-4">Dias reservados: Desde ${date_from} - Hasta ${date_to}</p>
           <p className="text-base mb-4">Cantidad de ocupantes reservados: ${occupants}</p>
           <p className="text-base mb-4">Si tienes alguna pregunta, no dudes en contactarnos. ¡Disfruta de tu experiencia!</p>
