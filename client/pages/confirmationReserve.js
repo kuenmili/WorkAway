1eae3dd980cfee150833ab33dec36fe6aebef475
kuenmili <122223943+kuenmili@users.noreply.github.com>
2023-07-06 11:35:42 -0300

    dark

--- client/pages/confirmationReserve.js
+++ client/pages/confirmationReserve.js
@@ -8,8 +8,8 @@ const ConfirmationMessage = () => {
       <>
         <Navbar/>  
         <div className="flex flex-col items-center justify-center min-h-screen -mt-32">
-          <h1 className="text-4xl font-bold mt-4 mb-4">Reserva confirmada</h1>
-          <p className="text-xl mt-4">Por favor, verifique su correo electrónico.</p>
+          <h1 className="text-4xl font-bold mt-4 mb-4 dark:text-black">Reserva confirmada</h1>
+          <p className="text-xl mt-4 dark:text-black">Por favor, verifique su correo electrónico.</p>
         </div>
         <Footer />
       </>
